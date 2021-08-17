7bb017f125ab0f716a2366c25887c567920012d0
Renato Andrade <renatorobot@outlook.com.br>
2021-08-17 10:36:27 -0300

    Criando repository e service para categoria

---
+++ src/main/java/com/renatochkai/cursomc/services/CategoriaService.java
@@ -0,0 +1,27 @@
+package com.renatochkai.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.renatochkai.cursomc.domain.Categoria;
+import com.renatochkai.cursomc.repositories.CategoriaRepository;
+
+
+@Service
+public class CategoriaService {
+
+	
+	@Autowired
+	private CategoriaRepository repo; 
+	
+	public Categoria buscar(Integer id) {
+		
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+		
+	}
+	
+	
+}
